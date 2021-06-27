478eed82a01f64d3c203a38e2161563b7d072269
Mohit Maheshwari <mohit0maheshwari@gmail.com>
2021-06-27 13:29:00 +0100

    Commit the ApplicationConstants file to update the messages.

--- creditcard/src/main/java/com/project/es/creditcard/constant/ApplicationConstants.java
+++ creditcard/src/main/java/com/project/es/creditcard/constant/ApplicationConstants.java
@@ -7,7 +7,7 @@ public class ApplicationConstants {
 	public static String INTERNAL_SERVER_ERROR = "Internal server error while performing operation";
 	public static String INVALID_CARD_NUMBER = "Please enter valid card number which is not more than 19 characters.";
 	public static String EMPTY_FIELDS = "Please provide the valid value of the name and limit Field";
-	public static String CARD_NUMBER_ALREADY_EXIST  = "Card Number already exist. Please select another";
+	public static String CARD_NUMBER_ALREADY_EXIST  = "Card Number already exist. Please select another.";
 	public static String CARD_LUHN_VALIDATION  = "Card LUHN validation failed. Please enter valid card";
 
 }
